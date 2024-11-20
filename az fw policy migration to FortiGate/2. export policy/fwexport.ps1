a92b246ef5f332779f94c87caf073b062d96a2a6
FilipDjor <Filip.Djordjevic@delhaize.rs>
2024-11-20 08:51:25 +0100

    ps

--- az fw policy migration to FortiGate/2. export policy/fwexport.ps1
+++ az fw policy migration to FortiGate/2. export policy/fwexport.ps1
@@ -1,7 +1,7 @@
 #Provide Input. Firewall Policy Name, Firewall Policy Resource Group & Firewall Policy Rule Collection Group Name
-$fpname = "azfwpolicy-vwan-weeu-01"
-$fprg = "rg-connectivity-prd-weeu-01"
-$fprcgname = "DefaultNetworkRuleCollectionGroup"
+$fpname = "name-of-the-policy"
+$fprg = "name-of-the-resource-group"
+$fprcgname = "prcgname"
 
 $fp = Get-AzFirewallPolicy -Name $fpname -ResourceGroupName $fprg
 $rcg = Get-AzFirewallPolicyRuleCollectionGroup -Name $fprcgname -AzureFirewallPolicy $fp
